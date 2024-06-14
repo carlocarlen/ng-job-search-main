2af0f3188d5c4d95cf6d7fd8d04f382c7c8c2579
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-14 10:48:06 +0200

    job list item

---
+++ src/app/jobs/job-list-item/job-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-list-item.component.html',
+  styleUrl: './job-list-item.component.css'
+})
+export class JobListItemComponent {
+
+}
--- src/app/jobs/job-list/job-list.component.ts
+++ src/app/jobs/job-list/job-list.component.ts
@@ -3,13 +3,14 @@ import { Job } from '../job.model';
 import { JobsQueryService } from '../jobs-query.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { JobListItemComponent } from "../job-list-item/job-list-item.component";
 
 @Component({
-  selector: 'app-job-list',
-  standalone: true,
-  imports: [ CommonModule ],
-  templateUrl: './job-list.component.html',
-  styleUrl: './job-list.component.css'
+    selector: 'app-job-list',
+    standalone: true,
+    templateUrl: './job-list.component.html',
+    styleUrl: './job-list.component.css',
+    imports: [CommonModule, JobListItemComponent]
 })
 export class JobListComponent implements OnInit {
 
