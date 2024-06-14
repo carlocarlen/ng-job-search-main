fa5c16b65d35cc1d6194fb5651ec3aa5ccaee663
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-14 10:53:40 +0200

    Pass job to the item

--- src/app/jobs/job-list-item/job-list-item.component.ts
+++ src/app/jobs/job-list-item/job-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Job } from '../job.model';
 
 @Component({
   selector: 'app-job-list-item',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './job-list-item.component.css'
 })
 export class JobListItemComponent {
+
+  @Input() job!: Job;
 
 }
