93d275c349747116f8e914b9d012dbfc9a640c14
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-15 09:08:38 +0200

    Favorites service

--- src/app/favorites/favorites.service.spec.ts
+++ src/app/favorites/favorites.service.spec.ts
@@ -1,16 +1,112 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FavoritesService } from './favorites.service';
+import { LocalStorageService } from '../shared/local-storage.service';
+import { Job } from '../jobs/job.model';
+import { computed, effect } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { JobsQueryService } from '../jobs/jobs-query.service';
+
+const jobOne: Job = {
+  title: 'Job One',
+  companyName: 'Company One',
+  companyLogo: 'OneLogo',
+  reference: 'job-one'
+}
+const jobTwo: Job = {
+  title: 'Job Two',
+  companyName: 'Company two',
+  companyLogo: 'TwoLogo',
+  reference: 'job-two'
+}
 
 describe('FavoritesService', () => {
   let service: FavoritesService;
-
+ 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const jobsServiceMock = jasmine.createSpyObj('jobsService', ['getAllJobs']);
+    jobsServiceMock.getAllJobs.and.returnValue(of([jobOne, jobTwo]));
+
+    const localStorageServiceMock = new LocalStorageServiceMock();
+
+    TestBed.configureTestingModule({
+      providers: [
+        FavoritesService, 
+        {provide: LocalStorageService, useValue: localStorageServiceMock},
+        {provide: JobsQueryService, useValue: jobsServiceMock},
+      ]
+    });
+
     service = TestBed.inject(FavoritesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('addFavorite', () => {
+    it('should not erase previous values', () => {
+      service.addFavorite(jobOne);
+      expect(service.favorites().length).toBe(1);
+  
+      service.addFavorite(jobTwo);
+      expect(service.favorites().length).toBe(2);
+  
+    })
+  
+    it('should not add values twice', () => {
+      service.addFavorite(jobOne);
+      service.addFavorite(jobOne);
+      expect(service.favorites().length).toBe(1);
+    })   
+  })
+
+
+
+/*   it('addFavorite should make favorites to emit a new signal', (done) => {
+    let firstCall = true;
+    effect(() => {
+      if (firstCall && service.favorites().length == 1) {
+        firstCall = false;
+      } else if (service.favorites().length == 2) {
+        done();
+      } else {
+        done.fail();
+      }
+    });
+
+    service.addFavorite(jobOne);
+    service.addFavorite(jobTwo);
+
+  }) */
+
+  it('removeFavorite should remove only that favorite')
+
+  it('removeFavorite should make getFavorites to emit a new signal')
 });
+
+class LocalStorageServiceMock {
+
+  private localStorageMock = new Map();
+
+  // Set a value in local storage
+  setItem(key: string, value: string): void {
+    this.localStorageMock.set(key, value);
+  }
+
+  // Get a value from local storage
+  getItem(key: string): string | null {
+    return this.localStorageMock.get(key);
+  }
+
+  // Remove a value from local storage
+  removeItem(key: string): void {
+    this.localStorageMock.delete(key);
+  }
+
+  // Clear all items from local storage
+  clear(): void {
+    this.localStorageMock.clear();
+  }
+  
+}
--- src/app/favorites/favorites.service.ts
+++ src/app/favorites/favorites.service.ts
@@ -1,27 +1,41 @@
-import { Injectable, Signal, signal } from '@angular/core';
+import { Injectable, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop'
 import { Job } from '../jobs/job.model';
+import { LocalStorageService } from '../shared/local-storage.service';
+import { JobsQueryService } from '../jobs/jobs-query.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
+  static readonly FAVORITES = "FAVORITES";
+  static readonly SEPARATOR = ",";
+  
+  favorites: Signal<Job[]>;
 
-  constructor() { }
+  private favoriteKeys: WritableSignal<Set<string>> = signal(new Set());
 
-  /**
-   * Using signals instead of Observables, as recommended.
-   * @returns a Signal to get the list of favorites jobs. The signal emits each time the list changes.
-   */
-  getFavorites(): Signal<Job[]> {
-    return signal([]);
-  }
+  constructor(
+    private localStorageService: LocalStorageService,
+    jobsService: JobsQueryService,
+  ) {
+    const allJobs: Signal<Job[]> = toSignal(jobsService.getAllJobs(), {initialValue: []});
+    this.favorites = computed(() => {
+      // Not sure if this is necessary, to subscribe instead of directly in the filter
+      let currentKeys = this.favoriteKeys();
+      return allJobs().filter(job => currentKeys.has(job.reference));
+    })
+   }
 
   /**
    * Add a job to the favorites
    * @param job a favorite job
    */
   addFavorite(job: Job) {
-
+    const jobKey = this.jobToKey(job);
+    let newKeys: Set<string> = this.getStoredKeys().add(jobKey);
+    this.localStorageService.setItem(FavoritesService.FAVORITES, this.setToString(newKeys));
+    this.favoriteKeys.set(newKeys)
   }
 
   /**
@@ -30,5 +44,22 @@ export class FavoritesService {
    */
   removeFavorite(job: Job) {
 
+  }
+
+  private getStoredKeys(): Set<string> {
+    const storedString = this.localStorageService.getItem(FavoritesService.FAVORITES) ?? "";
+    return this.stringToSet(storedString);
+  }
+
+  private jobToKey(job: Job): string {
+    return job.reference;
+  }
+
+  private stringToSet(input: string): Set<string> {
+    return input.length == 0 ? new Set<string>() : new Set<string>(input.split(FavoritesService.SEPARATOR));
+  }
+
+  private setToString(input: Set<string>): string {
+    return Array.from(input).join(FavoritesService.SEPARATOR);
   }
 }
