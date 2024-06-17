14b13b8ea9d49fc89746e8e85103924745e91ebf
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-17 16:08:51 +0200

    Go back works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { AllJobsTabComponent } from './jobs/all-jobs-tab/all-jobs-tab.component'
 import { JobDetailsComponent } from './jobs/job-details/job-details.component';
 
 export const routes: Routes = [
-    { path: 'jobs', component: AllJobsTabComponent},
-    { path: 'favorites', component: FavoritesTabComponent},
-    { path: '', redirectTo: '/jobs', pathMatch: 'full'},
-    { path: 'jobs/:jobId', component: JobDetailsComponent},
+    { path: 'jobs', component: AllJobsTabComponent },
+    { path: 'favorites', component: FavoritesTabComponent },
+    { path: '', redirectTo: '/jobs', pathMatch: 'full' },
+    { path: 'jobs/:jobId', component: JobDetailsComponent },
+    { path: 'favorites/:jobId', component: JobDetailsComponent },
 ];
