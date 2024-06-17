3115fbb487d4cbffedd2cdcd15bad689b2a3cb79
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-17 17:03:06 +0200

    Decent display of details

--- src/app/jobs/job-details-content/job-details-content.component.ts
+++ src/app/jobs/job-details-content/job-details-content.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { JobDetail } from '../job.model';
 
 @Component({
   selector: 'app-job-details-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job-details-content.component.html',
   styleUrl: './job-details-content.component.css'
 })
