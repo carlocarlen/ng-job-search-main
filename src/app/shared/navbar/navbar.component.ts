d666c8cf09d2b8776cc79ea0240741ccc8cb04f2
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-18 08:33:33 +0200

    routerlinkActive

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  currentTab: string = 'jobs'; // Default to 'jobs' tab
 
-  setCurrentTab(tabName: string): void {
-    this.currentTab = tabName;
-  }
 }
